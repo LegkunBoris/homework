05695c83b0627fd441f24f0a33146c19010fb101
LegkunBoris <dizamino@gmail.com>
2017-10-31 17:01:17 +0300

    HomeWork1

---
+++ HomeWork1/MyApp/Program.cs
@@ -0,0 +1,11 @@
+﻿using MyLib;
+namespace MyApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintLib.PrintMyMessage("Hello World!");
+        }
+    }
+}
---
+++ HomeWork1/MyLib/PrintLib.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace MyLib
+{
+    public class PrintLib
+    {
+        public static void PrintMyMessage(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
